addb3998dd968f995f0a78f20e203cc429a6209c
ella <yanzi19841227@gmail.com>
2022-08-30 21:09:55 +0800

    add product.html

--- js/_tooltip.js
+++ js/_tooltip.js
@@ -1,30 +1,30 @@
+$(function () {
+    $('[data-toggle="tooltip"]').click(function (event) {
+        event.preventDefault();
+    });
 
-
-$(function() {
-  $('[data-toggle="tooltip"]').click(function(event){
-    event.preventDefault();
-  });
-
-  $(document).tooltip({
-    // 內容 - title
-    content: function() {
-      var element = $(this);
-      if ( element.is("[title]" )) {
-        return element.attr("title");
-      }
-    },
-    // 位置
-    position: {
-      my: "center bottom-20",
-      at: "center top",
-      using: function(position, feedback) {
-        $(this).css(position);
-        $("<div>")
-          .addClass("ui-tooltip_arrow")
-          .addClass(feedback.vertical)
-          .addClass(feedback.horizontal)
-          .appendTo(this);
-      }
-    }
-  });
+    $(document).tooltip({
+        // 內容 - title
+        content: function () {
+            const element = $(this);
+            if (element.is('[title]')) {
+                return element.attr('title');
+            }
+        },
+        // 位置
+        position: {
+            my: 'center bottom-20',
+            at: 'center top',
+            using: function (position, feedback) {
+                console.log(position);
+                console.log(feedback);
+                $(this).css(position);
+                $('<div>')
+                    .addClass('ui-tooltip_arrow')
+                    .addClass(feedback.vertical)
+                    .addClass(feedback.horizontal)
+                    .appendTo(this);
+            }
+        }
+    });
 });
